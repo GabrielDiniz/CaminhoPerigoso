9b15efc3b37d38ef37c7d1cb37944dd7fbb8792d
Gabriel Diniz <contato@gabrieldiniz.com>
2012-06-02 04:54:39 -0300

    melhorando regras da marcação de vizinhos

--- marcaVizinhos.pl
+++ marcaVizinhos.pl
@@ -1,256 +1,221 @@
 marcarVizinhos( [ X, Y ], Sentido ) :- 
+	(
 	posicaoValida( [X,Y] ),
 	Sentido == ( nada ),
 	definePosicao([X+1,Y], '.'),
 	definePosicao([X,Y+1], '.'),
 	definePosicao([X-1,Y], '.'),
-	definePosicao([X,Y-1], '.').
+	definePosicao([X,Y-1], '.')
 
-
-
-
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X-1,Y])),
 	not(posicaoValida([X,Y+1])),
 	mundo([X,Y-1],'.'),
-	definePosicao([X+1,Y], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X+1,Y], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X-1,Y])),
 	not(posicaoValida([X,Y+1])),
 	mundo([X+1,Y],'.'),
-	definePosicao([X,Y-1], 'P').
+	definePosicao([X,Y-1], 'P')
 
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X-1,Y])),
 	not(posicaoValida([X,Y-1])),
 	mundo([X+1,Y],'.'),
-	definePosicao([X,Y+1], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X,Y+1], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X-1,Y])),
 	not(posicaoValida([X,Y-1])),
 	mundo([X,Y+1],'.'),
-	definePosicao([X+1,Y], 'P').
+	definePosicao([X+1,Y], 'P')
 
 
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X+1,Y])),
 	not(posicaoValida([X,Y-1])),
 	mundo([X,Y+1],'.'),
-	definePosicao([X-1,Y], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X-1,Y], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X+1,Y])),
 	not(posicaoValida([X,Y-1])),
 	mundo([X-1,Y],'.'),
-	definePosicao([X,Y+1], 'P').
+	definePosicao([X,Y+1], 'P')
 
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X+1,Y])),
 	not(posicaoValida([X,Y+1])),
 	mundo([X-1,Y],'.'),
-	definePosicao([X,Y-1], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X,Y-1], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X+1,Y])),
 	not(posicaoValida([X,Y+1])),
 	mundo([X,Y-1],'.'),
-	definePosicao([X-1,Y], 'P').
+	definePosicao([X-1,Y], 'P')
 
 
-
-
-
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X,Y+1])),
 	mundo([X,Y-1],'.'),
 	mundo([X-1,Y],'.'),
-	definePosicao([X+1,Y], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X+1,Y], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X,Y+1])),
 	mundo([X,Y-1],'.'),
 	mundo([X+1,Y],'.'),
-	definePosicao([X-1,Y], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X-1,Y], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X,Y+1])),
 	mundo([X+1,Y],'.'),
 	mundo([X-1,Y],'.'),
-	definePosicao([X,Y-1], 'P').
+	definePosicao([X,Y-1], 'P')
 
 
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X+1,Y])),
 	mundo([X,Y-1],'.'),
 	mundo([X-1,Y],'.'),
-	definePosicao([X,Y+1], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X,Y+1], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X+1,Y])),
 	mundo([X,Y-1],'.'),
 	mundo([X,Y+1],'.'),
-	definePosicao([X-1,Y], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X-1,Y], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X+1,Y])),
 	mundo([X-1,Y],'.'),
 	mundo([X,Y+1],'.'),
-	definePosicao([X,Y-1], 'P').
+	definePosicao([X,Y-1], 'P')
 
 
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X,Y-1])),
 	mundo([X-1,Y],'.'),
 	mundo([X+1,Y],'.'),
-	definePosicao([X,Y+1], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X,Y+1], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X,Y-1])),
 	mundo([X-1,Y],'.'),
 	mundo([X,Y+1],'.'),
-	definePosicao([X+1,Y], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X+1,Y], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X,Y-1])),
 	mundo([X+1,Y],'.'),
 	mundo([X,Y+1],'.'),
-	definePosicao([X-1,Y], 'P').
+	definePosicao([X-1,Y], 'P')
 
 
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X-1,Y])),
 	mundo([X+1,Y],'.'),
 	mundo([X,Y-1],'.'),
-	definePosicao([X,Y+1], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X,Y+1], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X-1,Y])),
 	mundo([X,Y+1],'.'),
 	mundo([X,Y-1],'.'),
-	definePosicao([X+1,Y], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X+1,Y], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	not(posicaoValida([X-1,Y])),
 	mundo([X,Y+1],'.'),
 	mundo([X+1,Y],'.'),
-	definePosicao([X,Y-1], 'P').
+	definePosicao([X,Y-1], 'P')
 
 
 
 
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	mundo([X,Y-1],'.'),
 	mundo([X,Y+1],'.'),
 	mundo([X+1,Y],'.'),
-	definePosicao([X-1,Y], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X-1,Y], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	mundo([X-1,Y],'.'),
 	mundo([X,Y+1],'.'),
 	mundo([X+1,Y],'.'),
-	definePosicao([X,Y-1], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X,Y-1], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	mundo([X,Y-1],'.'),
 	mundo([X,Y+1],'.'),
 	mundo([X-1,Y],'.'),
-	definePosicao([X+1,Y], 'P').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X+1,Y], 'P')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	mundo([X,Y-1],'.'),
 	mundo([X-1,Y],'.'),
 	mundo([X+1,Y],'.'),
-	definePosicao([X,Y+1], 'P').
+	definePosicao([X,Y+1], 'P')
 
 
 %--------------------------------------------------------------------------------------%
 
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	mundo([X,Y-1],'?'),
-	definePosicao([X,Y-1], 'P?').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X,Y-1], 'P?')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	mundo([X,Y+1],'?'),
-	definePosicao([X,Y+1], 'P?').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X,Y+1], 'P?')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	mundo([X-1,Y],'?'),
-	definePosicao([X-1,Y], 'P?').
-
-marcarVizinhos( [ X, Y ], Sentido ) :- 
+	definePosicao([X-1,Y], 'P?')
+	);(
 	posicaoValida( [X,Y] ),
 	Sentido == ( brisa ),
 	mundo([X+1,Y],'?'),
-	definePosicao([X+1,Y], 'P?').
-
-
-
+	definePosicao([X+1,Y], 'P?')
+	).
 %--------------------------------------------------------------------------------------%
 
 
